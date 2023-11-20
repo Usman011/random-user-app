d12687f5eaf3565dc4489e6f6c3c9e6ae7214bb1
Usman Nasir <m.usmannasir011@gmail.com>
2023-11-20 05:13:23 +0500

    adds gender and limit filter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   Box,
   CircularProgress,
@@ -7,22 +7,22 @@ import {
   Select,
   Typography,
   SelectChangeEvent,
-} from '@mui/material';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
-import { getUserList } from 'services/api';
-import { Flex } from 'components/layout';
-import UserLists from 'components/UserLists';
-import PaginationButton from 'components/PaginationButton';
-import ErrorSnackbar from 'components/ErrorSnackbar';
+} from "@mui/material";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import { getUserList } from "services/api";
+import { Flex } from "components/layout";
+import UserLists from "components/UserLists";
+import PaginationButton from "components/PaginationButton";
+import ErrorSnackbar from "components/ErrorSnackbar";
 
 const Home = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [limit, setLimit] = useState("4");
   const [gender, setGender] = useState<string>("male");
   const { data, error, isFetching, isPlaceholderData } = useQuery({
-    queryKey: ["projects", currentPage, gender],
-    queryFn: () => getUserList({currentPage, gender}),
+    queryKey: ["projects", currentPage, gender, limit],
+    queryFn: () => getUserList({ currentPage, gender, limit }),
     placeholderData: keepPreviousData,
-    staleTime: 5000,
   });
   const handleChange = (page: number) => {
     setCurrentPage(page);
@@ -32,24 +32,39 @@ const Home = () => {
     setGender(e.target.value as string);
   };
 
+  const handleLimitChange = (e: SelectChangeEvent) => {
+    setLimit(e.target.value as string);
+  };
+
   return (
     <Container>
       <Box py={4}>
-        <Typography variant="h3" fontWeight="bold" textAlign="center">
+        <Typography variant="h4" fontWeight="bold" textAlign="center" mb={2}>
           Random User List
         </Typography>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={gender}
-          label="Age"
-          onChange={handleGenderChange}
-          
-        >
-          <MenuItem value="male">Male</MenuItem>
-          <MenuItem value="female">Female</MenuItem>
-          <MenuItem value="both">Both</MenuItem>
-        </Select>
+        <Flex justifyContent="space-between">
+          <Box>
+            <Typography variant="subtitle1" color="primary">
+              Select Gender
+            </Typography>
+            <Select value={gender} label="Gender" onChange={handleGenderChange}>
+              <MenuItem value="male">Male</MenuItem>
+              <MenuItem value="female">Female</MenuItem>
+              <MenuItem value="both">Both</MenuItem>
+            </Select>
+          </Box>
+          <Box>
+            <Typography variant="subtitle1" color="primary">
+              Select Limit
+            </Typography>
+            <Select value={limit} label="Limit" onChange={handleLimitChange}>
+              <MenuItem value="4">4</MenuItem>
+              <MenuItem value="8">8</MenuItem>
+              <MenuItem value="12">12</MenuItem>
+            </Select>
+          </Box>
+        </Flex>
+
         <UserLists data={data?.results} />
 
         <Flex justifyContent="space-between" my={4}>
--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,7 @@ import { User } from "components/UserLists/types";
 interface getUserListProps {
   currentPage: number;
   gender: string;
+  limit: string
 }
 
 interface UserListResponse {
@@ -17,9 +18,10 @@ interface UserListResponse {
 export const getUserList = async ({
   currentPage,
   gender,
+  limit
 }: getUserListProps): Promise<UserListResponse> => {
   const response = await fetch(
-    `https://randomuser.me/api/?page=${currentPage}&results=4&gender=${gender}`
+    `https://randomuser.me/api/?page=${currentPage}&results=${limit}&gender=${gender}`
   );
   const data = await response.json();
   return data;
